27662a60f0ddb8c8fff1fa5b033df9f0b9a3cab8
Aviral Kumar <95560494+Apilex100@users.noreply.github.com>
2026-07-05 10:14:45 +0530

    Update compression.h

--- source/compression.h
+++ source/compression.h
@@ -3,7 +3,11 @@ Author : Apilex100
 Date : 2023-July-06
 */
 
-#define MAX 16
+// Max Huffman codeword length: for byte-frequency Huffman on up to 256 distinct
+// symbols the longest codeword is at most 255 bits, so size the buffer to 256
+// (plus room for the terminating '\0'). This prevents buffer overflows when
+// codewords are copied into codeTable.code for skewed/large inputs.
+#define MAX 256
 // padding is done to ensure that the code generated for each charater will fit byte size.
 // i.e : 4 byte + 3bits will be consider as 5 bits.
 char padding;
